167653e33ca026a10735cd9e0fba80117e8eaf43
Gabriella Shirkova <gabriellashirkova@gmail.com>
2018-05-27 10:59:59 +0300

    Exercises

---
+++ Interval Tree, Quad-Tree,K-d Tree/Interval Tree/IntervalTree/IntervalTree.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class IntervalTree
+{
+    private class Node
+    {
+        internal Interval Interval;
+        internal double Max;
+        internal Node Right;
+        internal Node Left;
+
+        public Node(Interval interval)
+        {
+            this.Interval = interval;
+            this.Max = interval.Hi;
+        }
+    }
+
+    private Node root;
+
+    public void Insert(double lo, double hi)
+    {
+        this.root = this.Insert(this.root, lo, hi);
+    }
+
+    public void EachInOrder(Action<Interval> action)
+    {
+        EachInOrder(this.root, action);
+    }
+
+    public Interval SearchAny(double lo, double hi)
+    {
+        Node node = this.root;
+        while (node != null && !node.Interval.Intersects(lo, hi))
+        {
+            if (node.Left != null && node.Left.Max > lo)
+                node = node.Left;
+            else
+                node = node.Right;
+        }
+
+        if (node != null)
+            return node.Interval;
+
+        return null;
+    }
+
+    public IEnumerable<Interval> SearchAll(double lo, double hi)
+    {
+        var result = new List<Interval>();
+        this.SearchAll(this.root, lo, hi, result);
+        return result;
+    }
+
+    private void SearchAll(Node node, double lo, double hi, List<Interval> result)
+    {
+        if (node == null)
+            return;
+
+        bool goingLeftPossible = node.Left != null && node.Left.Max > lo;
+        bool goingRightPossible = node.Right != null && node.Right.Interval.Lo < hi;
+
+        if (goingLeftPossible)
+            this.SearchAll(node.Left, lo, hi, result);
+
+        if (node.Interval.Intersects(lo, hi))
+            result.Add(node.Interval);
+
+        if (goingRightPossible)
+            this.SearchAll(node.Right, lo, hi, result);
+    }
+
+    private void UpdateMaxEndpoint(Node node)
+    {
+        var maxChild = GetMax(node.Left, node.Right);
+        node.Max = GetMax(node, maxChild).Max;
+    }
+
+    private Node GetMax(Node left, Node right)
+    {
+        if (left == null)
+            return right;
+
+        else if (right == null)
+            return left;
+
+        else
+            return left.Max > right.Max ? left : right;
+    }
+
+    private void EachInOrder(Node node, Action<Interval> action)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        EachInOrder(node.Left, action);
+        action(node.Interval);
+        EachInOrder(node.Right, action);
+    }
+
+    private Node Insert(Node node, double lo, double hi)
+    {
+        if (node == null)
+        {
+            return new Node(new Interval(lo, hi));
+        }
+
+        int cmp = lo.CompareTo(node.Interval.Lo);
+        if (cmp < 0)
+        {
+            node.Left = Insert(node.Left, lo, hi);
+        }
+        else if (cmp > 0)
+        {
+            node.Right = Insert(node.Right, lo, hi);
+        }
+
+        return node;
+    }
+}
---
+++ Interval Tree, Quad-Tree,K-d Tree/K-d Tree/KdTree/KdTree.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+public class KdTree
+{
+    private Node root;
+
+    public class Node
+    {
+        public Node(Point2D point)
+        {
+            this.Point = point;
+        }
+
+        public Point2D Point { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+    }
+
+    public Node Root
+    {
+        get
+        {
+            return this.root;
+        }
+    }
+
+    public bool Contains(Point2D point)
+    {
+        Node current = this.root;
+        int depth = 0;
+
+        while(current != null)
+        {
+            int compare = current.Point.CompareTo(point);
+
+            if (compare == 0)
+                return true;
+            
+            if(depth % 2 == 0)
+            {
+                int cmpX = current.Point.X.CompareTo(point.X);
+
+                if (cmpX > 0)
+                    current = current.Left;
+
+                else
+                    current = current.Right;
+            }
+
+            else
+            {
+                int cmpY = current.Point.Y.CompareTo(point.Y);
+
+                if (cmpY > 0)
+                    current = current.Left;
+
+                else
+                    current = current.Right;
+            }
+            depth++;
+        }
+        return current != null;
+    }
+
+
+    public void Insert(Point2D point)
+    {
+        this.root = this.Insert(this.root, point, 0);
+    }
+
+    private Node Insert(Node node, Point2D point, int depth)
+    {
+        if (node == null)
+            return new Node(point);
+
+        if (depth % 2 == 0)
+        {
+            int compareX = node.Point.X.CompareTo(point.X);
+
+            if (compareX > 0)
+                node.Left = this.Insert(node.Left, point, depth + 1);
+
+            else
+                node.Right = this.Insert(node.Right, point, depth + 1);
+        }
+
+        else
+        {
+            int compareY = node.Point.Y.CompareTo(point.Y);
+
+            if (compareY > 0)
+                node.Left = this.Insert(node.Left, point, depth + 1);
+
+            else
+                node.Right = this.Insert(node.Right, point, depth + 1);
+        }
+
+        return node;
+    }
+
+    private int Compare(Point2D a, Point2D b, int depth)
+    {
+        int cmp = 0;
+        if (depth % 2 == 0)
+        {
+            cmp = a.X.CompareTo(b.X);
+            if (cmp == 0)
+            {
+                cmp = a.Y.CompareTo(b.Y);
+            }
+
+            return cmp;
+        }
+
+        else
+        {
+            cmp = a.Y.CompareTo(b.Y);
+            if (cmp == 0)
+            {
+                cmp = a.X.CompareTo(b.X);
+            }
+        }
+        return cmp;
+    }
+
+    public void EachInOrder(Action<Point2D> action)
+    {
+        this.EachInOrder(this.root, action);
+    }
+
+    private void EachInOrder(Node node, Action<Point2D> action)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        this.EachInOrder(node.Left, action);
+        action(node.Point);
+        this.EachInOrder(node.Right, action);
+    }
+}
